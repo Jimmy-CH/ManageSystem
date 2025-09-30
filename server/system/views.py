87bf8b9cf04154ec6ca5cbf465856007dedad3ab
陈光 <03420092@yto.net.cn>
2025-09-30 10:58:45 +0800

    feat: system api

--- server/system/views.py
+++ server/system/views.py
@@ -1,54 +1,34 @@
+# views.py
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import SystemConfig, Menu, StorageConfig
+from .serializers import SystemConfigSerializer, MenuSerializer, StorageConfigSerializer
+from .filters import SystemConfigFilter, MenuFilter, StorageConfigFilter
 
-from system.models import SystemConfig, Menu, StorageConfig
-from system.serializers import SystemConfigSerializer, MenuSerializer, StorageConfigSerializer
-from rest_framework.response import Response
 
-
-class ConfigViewSet(viewsets.ModelViewSet):
+class SystemConfigViewSet(viewsets.ModelViewSet):
     queryset = SystemConfig.objects.all()
     serializer_class = SystemConfigSerializer
-    permission_classes = [IsAdminUser]
-
-    @action(detail=False, methods=['get'])
-    def group(self, request):
-        """按分组返回配置"""
-        group = request.query_params.get('group', 'basic')
-        configs = SystemConfig.objects.filter(group=group)
-        return Response({item.key: item.value for item in configs})
-
-    @action(detail=False, methods=['post'])
-    def batch_save(self, request):
-        """批量保存配置"""
-        data = request.data
-        for key, value in data.items():
-            SystemConfig.objects.filter(key=key).update(value=value)
-        return Response({"message": "保存成功"})
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = SystemConfigFilter
+    search_fields = ['key', 'label']
+    ordering_fields = ['id']
 
 
 class MenuViewSet(viewsets.ModelViewSet):
-    queryset = Menu.objects.filter(parent__isnull=True).order_by('order')
+    queryset = Menu.objects.filter(parent__isnull=True)  # 只查根节点，子节点由序列化器递归
     serializer_class = MenuSerializer
-    permission_classes = [IsAdminUser]
-
-    def list(self, request, *args, **kwargs):
-        # 返回树形结构
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = MenuFilter
+    search_fields = ['title']
+    ordering_fields = ['order']
 
 
 class StorageConfigViewSet(viewsets.ModelViewSet):
     queryset = StorageConfig.objects.all()
     serializer_class = StorageConfigSerializer
-    permission_classes = [IsAdminUser]
-
-    @action(detail=True, methods=['post'])
-    def set_default(self, request, pk=None):
-        StorageConfig.objects.filter(is_default=True).update(is_default=False)
-        storage = self.get_object()
-        storage.is_default = True
-        storage.save()
-        return Response({"message": "默认存储设置成功"})
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = StorageConfigFilter
+    search_fields = ['name']
+    ordering_fields = ['id']
