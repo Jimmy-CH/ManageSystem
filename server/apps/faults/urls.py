4dab796ccbaef463a06e0fac23c422b5142b8d69
陈光 <03420092@yto.net.cn>
2025-12-11 09:20:02 +0800

    feat:故障统计

--- server/apps/faults/urls.py
+++ server/apps/faults/urls.py
@@ -11,5 +11,16 @@ router.register(r'time-specials', views.EventTimeSpecialViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('fault_statistics_data/', views.fault_statistics_data),                                   # 故障事件统计
+    path('fault_statistics_device_data/', views.fault_statistics_device_data),                     # 故障设备统计
+    path('fault_statistics_maintenance_data/', views.fault_statistics_maintenance_data),           # 故障维护统计
+    path('fault_statistics_level_data/', views.fault_statistics_level_data),                       # 故障等级统计
+    path('fault_statistics_impact_project_data/', views.fault_statistics_impact_project_data),     # 故障影响项目统计
+    path('fault_statistics_category_trend_data/', views.fault_statistics_category_trend_data),     # 故障分类趋势统计
+    path('fault_statistics_device_unit_data/', views.fault_statistics_device_unit_data),           # 故障设备单元统计
+    path('fault_statistics_annual_data/', views.fault_statistics_annual_data),                     # 故障年度统计
+    path('maintenance_statistics_data/', views.maintenance_statistics_data),                       # 维护商统计
+    path('maintenance_statistics_score_data/', views.maintenance_statistics_score_data),           # 维护商评分统计
+    path('maintenance_statistics_score_table_data/', views.maintenance_statistics_score_table_data),  # 维护商评分表格统计
 ]
 
--- server/apps/faults/utils.py
+++ server/apps/faults/utils.py
@@ -1,3 +1,4 @@
+import datetime
 from apps.faults.models import Event
 
 
@@ -35,4 +36,30 @@ def build_fault_context(event: Event) -> dict:
 
     return data
 
+
+def get_error_time(error_time_list):
+    for i in error_time_list:
+        for j in error_time_list:
+            if i['start_time'] < j['start_time'] < i['end_time']:
+                i['end_time'] = j['end_time']
+                j['start_time'], j['end_time'] = 0, 0
+    all_start_time = sum([i['start_time'] for i in error_time_list])
+    all_end_time = sum([i['end_time'] for i in error_time_list])
+    all_error_time = all_end_time - all_start_time
+    return all_error_time
+
+
+def get_last_time(query_type, time_range):
+    now = datetime.datetime.now()
+    dt = datetime.datetime.fromtimestamp(time_range[1])
+    if query_type == 'week':
+        return int(time_range[0]) - 86400 * 7, int(time_range[0])
+    elif query_type == 'month':
+        if dt.month == 1:
+            start = now.replace(year=now.year - 1, month=12, day=1)
+        else:
+            start = now.replace(month=now.month - 1, day=1)
+        return int(start.timestamp()), time_range[0]
+    elif query_type == 'year':
+        return now.replace(year=now.year - 1, month=1, day=1).timestamp(), time_range[0]
 
